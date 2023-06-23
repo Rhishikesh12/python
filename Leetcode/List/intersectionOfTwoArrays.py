81709e04b159cc584cd9a2700c44645c2c3802b9
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-06-23 14:38:30 +0530

    new

--- Leetcode/List/intersectionOfTwoArrays.py
+++ Leetcode/List/intersectionOfTwoArrays.py
@@ -12,3 +12,25 @@ class Solution:
             return findintersection(set1, set2)
         else:
             return findintersection(set2, set1)
+
+
+# Output = [3,5]
+
+
+# To print [3,3,5]
+def findIntersection(A, B):
+    intersection = []
+    i = 0
+    j = 0
+
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            intersection.append(A[i])
+            i += 1
+            j += 1
+        elif A[i] < B[j]:
+            i += 1
+        else:
+            j += 1
+
+    return intersection
