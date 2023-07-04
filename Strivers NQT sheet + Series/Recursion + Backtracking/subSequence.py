95f9d2802383885871e5326156cb1f856446440d
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-07-04 16:29:40 +0530

    pushed

--- Strivers NQT sheet + Series/Recursion + Backtracking/subSequence.py
+++ Strivers NQT sheet + Series/Recursion + Backtracking/subSequence.py
@@ -3,7 +3,29 @@
 2) SubSequences can be contiguous or non-contiguous, but subarrays are contiguous.
 3) subsequences follows array order.
 4) subarrys can be a subsequences.
+
+Given : 
+
+arr = [3,2,1]  return all the subSequences
+
 '''
+def subSequences(start, arr, res, n, subsequences):
+    if start == n:
+        subsequences.append(list(res))
+        return
 
-# power set problem
+    # pick and add the element to the subsequence
+    res.append(arr[start])
+    subSequences(start + 1, arr, res, n, subsequences)
+    res.pop()
 
+    # dont pick the element
+    subSequences(start + 1, arr, res, n, subsequences)
+
+
+arr = [3, 2, 1]
+subsequences = []
+subSequences(0, arr, [], len(arr), subsequences)
+
+for subseq in subsequences:
+    print(subseq)
